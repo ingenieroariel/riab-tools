096f0a8f4d4ba4e9ee622d53c78c41a1fc591fb3
Jeffrey Johnson <jjohnson@opengeo.org>
2011-02-24 17:17:54 -0800

    Initial add upload.py for uploading releases to S3

---
+++ upload.py
@@ -0,0 +1,15 @@
+import os, sys
+import boto
+from boto.s3.key import Key
+
+bucket_name = sys.argv[1] 
+file_name = sys.argv[2] 
+
+conn = boto.connect_s3(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'])
+bucket = conn.get_bucket(bucket_name)
+
+k = Key(bucket)
+k.key = file_name
+k.set_contents_from_filename(file_name)
+
+print file_name + " uploaded to " + bucket_name
